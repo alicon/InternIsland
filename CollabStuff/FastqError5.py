2df38e26b596d60c5c121bbf9e7a2fc362a3d45f
tindremene <sjohnson@archerdx.com>
2015-07-13 15:33:17 -0600

    This now clones on poisson distribution probability

--- CollabStuff/FastqError5.py
+++ CollabStuff/FastqError5.py
@@ -8,6 +8,7 @@ from __future__ import division
 
 __author__ = 'samanthajohnson'
 
+import numpy
 import random
 from sys import argv
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, FileType
@@ -15,7 +16,7 @@ from collections import defaultdict
 from colors import red, blue, green, yellow, magenta
 import math
 
-#Must install ansicolors module! Found at: https://pypi.python.org/pypi/ansicolors/1.0.2
+# Must install ansicolors module! Found at: https://pypi.python.org/pypi/ansicolors/1.0.2
 
 def add_error(error_seq_string, base, is_error=False):
     """Either changes the base to be an error or not. Adds to error_seq_string
@@ -31,17 +32,17 @@ def add_error(error_seq_string, base, is_error=False):
     :param boolean is_error: defaults false
     :return: str error_seq_string: modified error sequence'
     """
-    #asserttrue for both doctest and unittest put acceptable answers in a list
+    # asserttrue for both doctest and unittest put acceptable answers in a list
 
     if is_error:
-        #There's an error! Picks base from set of three not including the base that needs to change
+        # There's an error! Picks base from set of three not including the base that needs to change
         error_base = 'AGCTAG'
         error_base_index = random.randint(0, 2)
         if base == 'T':
-            #Add the new, error base to the error string out of the first set
+            # Add the new, error base to the error string out of the first set
             error_seq_string += red(error_base[error_base_index], style='bold')
         elif base == 'A':
-            #The '+ 1' is checking the second set of three bases
+            # The '+ 1' is checking the second set of three bases
             error_seq_string += red(error_base[error_base_index + 1], style='bold')
         elif base == 'G':
             error_seq_string += red(error_base[error_base_index + 2], style='bold')
@@ -50,6 +51,7 @@ def add_error(error_seq_string, base, is_error=False):
     else:
         error_seq_string += base
     return error_seq_string
+
 
 def print_errors(seq_name, seq_string, error_seq_string, error_num):
     """Prints all of the data nicely
@@ -70,25 +72,25 @@ def print_errors(seq_name, seq_string, error_seq_string, error_num):
     """
     print '{0} no error:      {1} \n' \
           '{0} random errors: {2} \n' \
-          'Number of random errors generated for {0}: {3}'\
+          'Number of random errors generated for {0}: {3}' \
         .format(seq_name, seq_string, error_seq_string, error_num)
 
-def create_fastq_dict(infile, n=1):
+
+def create_fastq_dict(infile):
     """Creates defaultdict of names of sequences as keys with sequences as values given a fastq file
 
     :param str infile: name of the file to be read in and made a dict of
     :return: defaultdict sequences: the dictionary with the names and sequences
     """
-    #infile = open(infile,'r')
+    # infile = open(infile,'r')
     sequences = defaultdict(str)
     for line in infile:
         line = line.rstrip()
         if line[0] == '@':
-            #Creates key of the name of the sequence and sets the value to the sequence
-            next_line = next(infile).rstrip()
-            for x in range(0, n):
-                sequences[line[1:].rstrip() + ' ' + str(x+1)] = next_line
+            # Creates key of the name of the sequence and sets the value to the sequence
+            sequences[line[1:].rstrip()] = next(infile).rstrip()
     return sequences
+
 
 def parse_cmdline_params(arg_list=None):
     """Parses command line arguments.
@@ -110,9 +112,10 @@ def parse_cmdline_params(arg_list=None):
                         help="Number of iterations",
                         required=False,
                         type=int)
-    #Parse options
+    # Parse options
     opts = parser.parse_args(args=arg_list)
     return opts
+
 
 def create_quality_scores(sequence):
     """Makes a string of ascii characters corresponding to different quality values.
@@ -121,7 +124,7 @@ def create_quality_scores(sequence):
     :return: str new_ascii: characters corresponding to phred scores determined by error prob
     """
     ascii_ref = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[/]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-    #print ascii_ref
+    # print ascii_ref
     position = 0
     new_ascii = ''
     for base in range(0, len(sequence)):
@@ -131,6 +134,36 @@ def create_quality_scores(sequence):
         new_ascii += ascii_ref[int(quality_score)]
     return new_ascii
 
+
+def cloning(infile, mean=1):
+    """Creates defaultdict of names of sequences as keys with sequences as values given a fastq file
+
+    :param str infile: name of the file to be read in and made a dict of
+    :return: defaultdict sequences: the dictionary with the names and sequences
+    """
+    #infile = open(infile,'r')
+    sequences = defaultdict(str)
+    for line in infile:
+        line = line.rstrip()
+        if line[0] == '@':
+            n = number_of_clones(mean)
+            #Creates key of the name of the sequence and sets the value to the sequence
+            next_line = next(infile).rstrip()
+            for x in range(0, n):
+                sequences[line[1:].rstrip() + ' ' + str(x+1)] = next_line
+    return sequences
+
+
+def number_of_clones(mean):
+    clone_number = numpy.random.poisson(mean, 1)
+    return clone_number
+
+
+def create_cloned_file(infile, mean):
+    for line in infile:
+        outfile = cloning(line,number_of_clones(mean))
+    return outfile
+
 def main(args):
     """Main method
 
@@ -138,9 +171,8 @@ def main(args):
     :return: None
     """
     opts = parse_cmdline_params(args[1:])
-    print opts.n
-    sequences = create_fastq_dict(opts.fastq, opts.n)
-    #sequences = create_fastq_dict('sampleFastq')
+    sequences = cloning(opts.fastq, opts.n)
+    # sequences = create_fastq_dict('sampleFastq')
     total_error_num = 0
     total_errors = 0
     total_lengths = 0
@@ -148,6 +180,7 @@ def main(args):
         error_seq_string = ''
         seq_name = key
         seq_string = sequences[key]
+
         position = 0
         error_num = 0
         for base in seq_string:
@@ -157,25 +190,25 @@ def main(args):
             error_prob = 0.001 * position
             if error_prob * 100 <= 0:
                 raise ValueError("The error probability must be greater than 0!")
-            #Checks if it needs to add an error
-            if random.randint(1, 1000) < (error_prob)*1000:
+            # Checks if it needs to add an error
+            if random.randint(1, 1000) < (error_prob) * 1000:
                 error_seq_string = add_error(error_seq_string, base, True)
                 error_num += 1
             else:
                 error_seq_string = add_error(error_seq_string, base, False)
         total_errors += error_num
         total_error_num += 1
-        error_perc = error_num/len(seq_string)
+        error_perc = error_num / len(seq_string)
         total_lengths += len(seq_string)
         print_errors(blue(seq_name), seq_string, error_seq_string, green(str(error_num)))
-        print 'The error percent for this sequence is:', yellow(str(error_perc*100)), '%'
+        print 'The error percent for this sequence is:', yellow(str(error_perc * 100)), '%'
         print 'The new quality string is:            ', create_quality_scores(seq_string), '\n'
     print 'The average number of errors generated was:' \
-          '', magenta(str(int(total_errors/total_error_num)))
-    print 'The total error percentage is:', magenta(str(total_errors/total_lengths*100)), '%'
+          '', magenta(str(int(total_errors / total_error_num)))
+    print 'The total error percentage is:', magenta(str(total_errors / total_lengths * 100)), '%'
 
 
 if __name__ == '__main__':
-    #import doctest
-    #doctest.testmod()
+    # import doctest
+    # doctest.testmod()
     main(argv)
